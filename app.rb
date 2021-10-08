45661a5dec6136ea0a66cde8e073b862b21d39aa
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-10-08 22:48:45 +0900

    SQLインジェクション対策をする処理を作成・app.rb/rubocop通過確認

--- app.rb
+++ app.rb
@@ -8,8 +8,12 @@ helpers do
     Rack::Utils.escape_html(text)
   end
 
-  def db_connection(table_select)
-    PG.connect(dbname: 'memo_db').exec(table_select)
+  def db_connect
+    PG.connect(dbname: 'memo_db')
+  end
+
+  def find_id_db_connection(table_select, *row)
+    db_connect.exec(table_select, row)
   end
 end
 
@@ -18,12 +22,13 @@ get '/' do
 end
 
 get '/memos' do
-  @datas = db_connection('SELECT * FROM Memo')
+  @datas = db_connect.exec('SELECT * FROM Memo')
   erb :index
 end
 
 post '/memos' do
-  db_connection("INSERT INTO Memo(content) VALUES ('#{params[:content]}')")
+  content = params[:content]
+  find_id_db_connection('INSERT INTO Memo(content) VALUES ($1) RETURNING id', content)
   redirect to '/memos'
 end
 
@@ -32,17 +37,21 @@ get '/new' do
 end
 
 get '/memos/:id/edit' do
-  @content = db_connection("SELECT * FROM Memo WHERE id='#{params[:id]}'").map { |row| row['content'].to_s }.join
+  id = params[:id]
   @id = params[:id]
+  @content = find_id_db_connection('SELECT * FROM Memo WHERE id=$1', id).map { |row| row['content'].to_s }.join
   erb :edit
 end
 
 patch '/memos/:id' do
-  db_connection("UPDATE Memo SET content = '#{params[:content]}' WHERE id='#{params[:id]}'")
+  content = params[:content]
+  id = params[:id]
+  find_id_db_connection('UPDATE Memo SET content=$1 WHERE id=$2;', content, id)
   redirect to '/memos'
 end
 
 delete '/memos/:id' do
-  db_connection("DELETE FROM Memo WHERE id='#{params[:id]}'")
+  id = params[:id]
+  find_id_db_connection('DELETE FROM Memo WHERE id=$1;', id)
   redirect to '/memos'
 end
